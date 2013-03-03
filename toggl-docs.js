26266c2d51c2315219dcf0ee1ded8b9fb79b2957
Koen <koen@koen-mint.(none)>
2013-03-03 21:43:44 +0100

    updates toggl-docs

--- toggl-docs.js
+++ toggl-docs.js
@@ -15,7 +15,7 @@
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-
+    Source located here: https://github.com/koen-github/Toggl-script-for-Google-Docs
 
 */
 function onOpen() {
